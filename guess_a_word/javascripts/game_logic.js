d8b975b13831249c0a99eedb5897b62cce8e38e1
Wendy <wendy.tsai1988@gmail.com>
2021-04-14 09:24:35 +0200

    add event listeners

--- guess_a_word/javascripts/game_logic.js
+++ guess_a_word/javascripts/game_logic.js
@@ -26,12 +26,24 @@
     when a letter is entered (keyup event?) : either display the letter at word container or guesses container; 
       increase incorrect count; or display all apples gone message (stop listening to key press)
 
-
+  bind keyup event:
+    - add event 
+    - verify if key is within a-z (if not, ignore)
+    - processing each guess: 
+      - check if word contains key (multiple )
+      - add letter to guessed letter array if not already guessed
+      - if letter matches: output each instance of the letter in word blanks (idx)
+      - if not match: write to guess container & increase incorrect guess count & change class name of apple container
+    - if incorrect guesses === 6, game over=> display message and a link to play again, unbind the keyup event
+    - if all letters are revealed, display win message and a link to play again, unbind keyup event
 */
 
 document.addEventListener('DOMContentLoaded', event => {
   let wordDiv = document.getElementById('spaces');
   let message = document.getElementById('message');
+  let replay = document.getElementById('replay');
+  let apples = document.getElementById('apples');
+  let guessDiv = document.getElementById('guesses');
 
 
   let randomWord = function() {
@@ -52,20 +64,26 @@ document.addEventListener('DOMContentLoaded', event => {
       }
     };
   }();
+
+  function inValidLetter(letter) {
+    let charCode = letter.charCodeAt();
+    return letter.length > 1 || charCode < 65 || charCode > 90;
+  }
   
-  
+
   class Game {
     constructor() {
       this.incorrectGuesses = 0;
-      this.gueesedLetters = [];
+      this.guessedLetters = [];
       this.allowedWrongGuesses = 6;
-
+     
       this.word = randomWord();
       if (!this.word) {
-        message.textContent = "Sorry, I've run out of words";
+        this.displayMessage("Sorry, I've run out of words");
         return this;
       }
       
+      this.word = this.word.toUpperCase().split('');
       this.init();
     }
 
@@ -75,17 +93,127 @@ document.addEventListener('DOMContentLoaded', event => {
       currentSpans.forEach(span => {
         span.parentNode.removeChild(span);
       });
-      
+
       for (let i = 0; i < this.word.length; i += 1) {
         wordDiv.appendChild(document.createElement('span'));
       }
+
+      this.wordSpans = wordDiv.querySelectorAll('span');
+    }
+
+    displayMessage(text) {
+      message.textContent = text;
+    }
+
+    duplicateGuess(letter) {
+      return this.guessedLetters.indexOf(letter) !== -1;
+    }
+
+    displayGuess(letter) {
+      let span = document.createElement('span');
+      span.textContent = letter;
+      guessDiv.appendChild(span);
+    }
+
+    processGuess(event) {
+      let letter = event.key.toUpperCase();
+      if (inValidLetter(letter) || this.duplicateGuess(letter)) { return; }
+      this.guessedLetters.push(letter);
+
+      if(this.word.includes(letter)) {
+        this.fillWordBlanks(letter);
+        let currentWordSpaces = this.renderCurrentWordSpaces();
+        if (this.word.join('') === currentWordSpaces.join('')) {
+          this.win();
+        }
+      } else {
+        this.displayGuess(letter);
+        this.incorrectGuesses += 1;
+        this.setApplesClass();
+
+        if (this.incorrectGuesses >= 6) {
+          this.lose();
+        }
+      }
+    }
+
+    fillWordBlanks(letter) {
+      let self = this;
+      self.word.forEach((char, idx) => {
+        if (letter === char) {
+          self.wordSpans[idx].textContent = letter;
+        }
+      });
+    }
+
+    renderCurrentWordSpaces() {
+      let self = this;
+      return Array.prototype.slice.call(self.wordSpans).map(sp => {
+        return sp.textContent;
+      })
+    }
+
+    win() {
+      this.unbindKeyEvent();
+      this.displayMessage("You win!");
+      this.showReplayLink();
+      this.setGameStatus('win');
+    }
+
+    lose() {
+      this.unbindKeyEvent();
+      this.displayMessage("Sorry! You're out of guesses");
+      this.showReplayLink();
+      this.setGameStatus('lose');
+    }
+
+    setGameStatus(status) {
+      let body = document.body;
+      body.classList.remove(body.classList[0]);
+      if (status) { body.classList.add(status); }
+
+    }
+
+    bindKeyEvent() {
+      this.processGuessHandler = (e) => this.processGuess(e);
+      document.addEventListener('keyup', this.processGuessHandler);
+    }
+
+
+    unbindKeyEvent() {
+      document.removeEventListener('keyup', this.processGuessHandler);
+    }
+
+    showReplayLink() {
+      replay.classList.add('visible');
+    }
+
+    hideReplayLink() {
+      replay.classList.remove('visible');
+    }
+
+    setApplesClass() {
+      apples.classList.remove(apples.classList[0]);
+      apples.classList.add("guess_" + this.incorrectGuesses);
     }
 
     init() {
       this.createBlanks();
+      this.hideReplayLink();
+      this.setApplesClass();
+      this.setGameStatus();
+      this.bindKeyEvent();
     }
   };
   
   new Game();
+
+  replay.addEventListener('click', event => {
+    event.preventDefault();
+    new Game();
+  });
 });
+
+
+
 
